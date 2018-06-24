1238c77216465d7b2be4eda988e6d143f74af817
Romulo San <romulossan@gmail.com>
2018-06-24 00:16:10 -0300

    ok

---
+++ Patterns/ObservedValues.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ObservedFloat{
+    [SerializeField]
+	private float observedValue;
+	public ChangeValueEvent onValueChanged;
+
+    [System.Serializable]
+    public class ChangeValueEvent: UnityEvent<float>{}
+
+    public float ObservedValue{
+        get{
+            return observedValue;
+        }set{
+			if(!observedValue.Equals(value))
+				onValueChanged.Invoke(value);
+            observedValue = value;
+        }
+    }
+
+	public void SilentSet(float value){
+		observedValue = value;
+	}
+}
+
+[System.Serializable]
+public class ObservedInt{
+    [SerializeField]
+	private int observedValue;
+	public ChangeValueEvent onValueChanged;
+
+    [System.Serializable]
+    public class ChangeValueEvent: UnityEvent<int>{}
+
+    public int ObservedValue{
+        get{
+            return observedValue;
+        }set{
+			if(!observedValue.Equals(value))
+				onValueChanged.Invoke(value);
+            observedValue = value;
+        }
+    }
+
+	public void SilentSet(int value){
+		observedValue = value;
+	}
+}
+
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	protected static T instance;	
+	public static T Instancia{
+		get{
+			while(instance == null){
+				instance = (T) FindObjectOfType(typeof(T));
+				//Debug.Log("procurou");	
+				if (instance == null){
+					//Debug.Log("criou");
+					GameObject obj = new GameObject();
+					//instancia = obj.AddComponent<T>();
+					obj.AddComponent<T>();	
+				}	
+			} return instance;
+		}
+	}	
+}
+
