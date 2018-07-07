7b3e1dd505cd3a18a1d55bce8d221d6a332ab3c9
Romulo San <romulossan@gmail.com>
2018-07-07 18:01:10 -0300

    ok

---
+++ Movement/RotByMoviment.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotByMoviment: MonoBehaviour{
+
+	public float multiplier=100;
+	public Vector3 axe;
+	
+	private Vector3  lastPos;
+
+	void Start(){
+		lastPos = transform.position;
+	}
+
+	void Update(){
+		CheckRotation();
+	}
+	
+	public  void CheckRotation(float sign =1){
+		float vel = sign * (lastPos -transform.position).magnitude * multiplier;
+		transform.Rotate(axe*vel,Space.Self);
+		lastPos = transform.position;
+	}
+}
