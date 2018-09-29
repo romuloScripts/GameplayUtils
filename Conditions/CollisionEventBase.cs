b178df9561e5e42bcd3770ec669cefc6c2b98db1
Romulo San <romulossan@gmail.com>
2018-09-29 18:36:10 -0300

    ok

--- Conditions/CollisionEventBase.cs
+++ Conditions/CollisionEventBase.cs
@@ -5,11 +5,11 @@ using UnityEngine.Events;
 
 public class CollisionEventBase : MonoBehaviour {
 
-	public UnityEvent onCollisionEnter;
 	public bool destroy=true;
 	public float delay=0;
 	public bool useTag;
-	public const string collisionTag="Player";
+	public string collisionTag="Player";
+	public UnityEvent onCollisionEnter;
 
 	protected bool triggered;
 
@@ -26,4 +26,14 @@ public class CollisionEventBase : MonoBehaviour {
 			Destroy(this);
 		}
 	}
+
+	public IEnumerator Collision2D(Collider2D col){ 
+		if((triggered && destroy) || (useTag && !col.CompareTag(collisionTag))) yield break; 
+		triggered = true; 
+		yield return new WaitForSeconds(delay); 
+		onCollisionEnter.Invoke(); 
+		if(destroy){ 
+			Destroy(this); 
+		} 
+	} 
 }
