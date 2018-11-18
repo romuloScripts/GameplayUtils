6dd2650d0d288da5bc01e922135bebd212d9fd16
Romulo San <romulossan@gmail.com>
2018-11-18 00:20:42 -0200

    ok

--- Movement/AutoSinCosMovement.cs
+++ Movement/AutoSinCosMovement.cs
@@ -7,20 +7,30 @@ public class AutoSinCosMovement : MonoBehaviour {
 	public float lerp=1;
 	public bool randomIni;
 	
-	Vector3 posIni, newPos;
+	Vector3 posIni, newPos, posGizmo;
 	float l,sum;
-	
+	bool ini;
+
 	void Start(){
 		newPos = Vector3.zero;		
 		posIni = transform.localPosition;
+		ini = true;
+		posGizmo = transform.position;
 		if(randomIni)
-			sum=Random.Range(-2f,7f);
+			sum=Random.Range(0f,10f);
 	}
 	
-	void FixedUpdate () {
-		newPos.x = Mathf.Sin(Mathf.PI * Time.time*vel.x+sum)*amplitude.x*l*Time.deltaTime;
-		newPos.y = Mathf.Cos(Mathf.PI * Time.time*vel.y+sum)*amplitude.y*l*Time.deltaTime;
-		transform.localPosition = posIni + newPos ;
+	void Update () {
+		newPos.x = Mathf.Sin(Mathf.PI * Time.time*vel.x+sum)*amplitude.x*l;
+		newPos.y = Mathf.Cos(Mathf.PI * Time.time*vel.y+sum)*amplitude.y*l;
+		transform.localPosition = posIni + newPos;
 		l = Mathf.Lerp(l,1,lerp*Time.deltaTime);
+	}
+
+	private void OnDrawGizmos() {
+
+		Gizmos.color = Color.yellow;
+		Gizmos.matrix = Matrix4x4.TRS(ini?posGizmo:transform.position,Quaternion.identity,amplitude);
+		Gizmos.DrawWireSphere(Vector3.zero,1);
 	}
 }
